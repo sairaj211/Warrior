5ee5c00700abadc4cd1c4fd8db0e89976c33f102
Sairaj Padghe <sairaj.padghe211@gmail.com>
2025-06-01 18:11:37 +0530

    Hit reaction added to enemies for light attack

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -2,6 +2,9 @@
 
 
 #include "AbilitySystem/WarriorAttributeSet.h"
+#include "GameplayEffectExtension.h"
+
+#include "WarriorDebugHelper.h"
 
 UWarriorAttributeSet::UWarriorAttributeSet()
 {
@@ -12,4 +15,46 @@ UWarriorAttributeSet::UWarriorAttributeSet()
 	InitMaxRage(1.f);
 	InitAttackPower(1.f);
 	InitDefensePower(1.f);
+}
+
+void UWarriorAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	if (Data.EvaluatedData.Attribute == GetCurrentHealthAttribute())
+	{
+		const float NewCurrentHealth = FMath::Clamp(GetCurrentHealth(),0.f,GetMaxHealth());
+		SetCurrentHealth(NewCurrentHealth);
+	}
+	
+	if (Data.EvaluatedData.Attribute == GetCurrentRageAttribute())
+	{
+		const float NewCurrentRage = FMath::Clamp(GetCurrentRage(),0.f,GetMaxRage());
+		SetCurrentRage(NewCurrentRage);
+	}
+
+	if (Data.EvaluatedData.Attribute == GetDamageTakenAttribute())
+	{
+		const float OldHealth = GetCurrentHealth();
+		const float DamageDone = GetDamageTaken();
+		const float NewCurrentHealth = FMath::Clamp(OldHealth - DamageDone,0.f,GetMaxHealth());
+
+		SetCurrentHealth(NewCurrentHealth);
+
+		const FString DebugString = FString::Printf(
+			TEXT("Old Health: %f, Damage Done: %f, NewCurrentHealth: %f"),
+			OldHealth,
+			DamageDone,
+			NewCurrentHealth
+		);
+
+		Debug::Print(DebugString,FColor::Green);
+
+		//TODO::Notify the UI 
+
+		//TODO::Handle character death
+		if (NewCurrentHealth == 0.f)
+		{
+			const FString DebugString1 = FString::Printf(TEXT("%s => DEAD"), *GetActorInfo()->OwnerActor->GetName());
+			Debug::Print(DebugString1,FColor::Red);
+		}
+	}
 }
--- Source/Warrior/Public/WarriorDebugHelper.h
+++ Source/Warrior/Public/WarriorDebugHelper.h
@@ -2,8 +2,12 @@
 
 namespace Debug 
 {
+	static bool bEnableDebugPrint = true;
+	
 	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 InKey = -1)
 	{
+		if (!bEnableDebugPrint) { return; }
+		
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(InKey, 7.0f, Color, Msg);
@@ -15,6 +19,8 @@ namespace Debug
 	
 	static void Print(const FString& FloatTitle, float FloatValueToPrint, int32 InKey = -1, const FColor& Color = FColor::MakeRandomColor())
 	{
+		if (!bEnableDebugPrint) { return; }
+		
 		if (GEngine)
 		{
 			const FString FinalMsg = FloatTitle + TEXT(": ") + FString::SanitizeFloat(FloatValueToPrint);
